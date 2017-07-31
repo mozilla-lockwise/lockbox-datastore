8d9c217bd051bf6d1c3f2ec537bea812a994426e
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-07-31 12:33:13 -0600

    JSDoc + trivial testing

--- test/datastore-test.js
+++ test/datastore-test.js
@@ -27,4 +27,42 @@ describe("datastore", () => {
     assert.strictEqual(result, main);
     assert.ok(main.locked);
   });
+
+  it("does basic CRUD ops", async () => {
+    // start by unlocking
+    await main.unlock();
+    let stored;
+    stored = await main.list();
+    assert.equal(stored.size, 0);
+
+    let something = {
+      title: "My Item",
+      entry: {
+        kind: "login",
+        username: "foo",
+        password: "bar"
+      }
+    };
+    let result = await main.add(something);
+    assert.deepNestedInclude(result, something);
+    stored = await main.list();
+    assert.equal(stored.size, 1);
+    assert.deepEqual(stored.get(result.id), result);
+
+    something = JSON.parse(JSON.stringify(result));
+    something.entry = Object.assign(something.entry, {
+      password: "baz"
+    });
+    result = await main.update(something);
+    assert.deepNestedInclude(result, something);
+    stored = await main.list();
+    assert.equal(stored.size, 1);
+    assert.deepEqual(stored.get(result.id), result);
+
+    something = result;
+    result = await main.remove(something.id);
+    assert.deepEqual(result, something);
+    stored = await main.list();
+    assert.equal(stored.size, 0);
+  });
 });
