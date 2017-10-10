530a7134a9a629905dc3edc7d91bdbd0da25833f
Matthew A. Miller <linuxwolf@outer-planes.net>
2017-10-10 10:58:51 -0600

    feature: add support for metrics reporting

--- test/datastore-test.js
+++ test/datastore-test.js
@@ -125,13 +125,27 @@ describe("datastore", () => {
   });
 
   describe("CRUD", () => {
-    let main, masterPwd;
+    let main, masterPwd, metrics;
+
+    function checkMetrics(expected) {
+      let actual = metrics;
+      metrics = [];
+
+      assert.equal(actual.length, expected.length);
+      for (let idx = 0; idx < expected.length; idx++) {
+        assert.deepEqual(actual[idx], expected[idx]);
+      }
+    }
 
     before(async () => {
-      localdatabase.startup();
+      await localdatabase.startup();
 
+      metrics = [];
       main = new DataStore({
-        keys: loadEncryptedKeys()
+        keys: loadEncryptedKeys(),
+        recordMetric: async (method, id, fields) => {
+          metrics.push({method, id, fields});
+        }
       });
       main = await main.prepare();
       masterPwd = loadMasterPassword();
@@ -174,6 +188,13 @@ describe("datastore", () => {
       cached.set(result.id, result);
       stored = await main.list();
       checkList(stored, cached);
+      checkMetrics([
+        {
+          method: "added",
+          id: result.id,
+          fields: undefined
+        }
+      ]);
 
       // result is the full item
       let expected = result;
@@ -192,6 +213,13 @@ describe("datastore", () => {
       cached.set(result.id, result);
       stored = await main.list();
       checkList(stored, cached);
+      checkMetrics([
+        {
+          method: "updated",
+          id: result.id,
+          fields: ["entry.password"]
+        }
+      ]);
 
       expected = result;
       result = await main.get(expected.id);
@@ -204,6 +232,13 @@ describe("datastore", () => {
       cached.delete(result.id);
       stored = await main.list();
       checkList(stored, cached);
+      checkMetrics([
+        {
+          method: "deleted",
+          id: result.id,
+          fields: undefined
+        }
+      ]);
 
       result = await main.get(result.id);
       assert(!result);
